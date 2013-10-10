ad9e0cc3cc78e664f4fd48ffd950642173031cbe
Erik <erik.w.gibbons@gmail.com>
2013-10-09 22:00:44 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Platesbook::Application.config.secret_key_base = '854c01d434e228b75501ab88b0d67db2921c8a55dfac1c1420be0a7271d0ba38c596484672d2207ed001bdeb4c7a00b4edba8cb33efab39fa091a6ff7033f18c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Platesbook::Application.config.session_store :cookie_store, key: '_platesbook_session'
