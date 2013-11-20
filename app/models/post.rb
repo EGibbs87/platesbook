1715ab8a5b4a1272d23d0b085db125cfe9c5fe6e
Erik <flyers_rock_87@hotmail.com>
2013-11-20 15:04:44 -0500

    Add post authorization and association with users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
 	attr_accessible :comment	
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131108015851) do
+ActiveRecord::Schema.define(version: 20131120193520) do
 
   create_table "posts", force: true do |t|
     t.string   "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
